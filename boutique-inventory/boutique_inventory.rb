22bfca7b181e68707f40b592509fb8b8fedeedbf
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-09-20 16:14:19 -0500

    progress made

--- boutique-inventory/boutique_inventory.rb
+++ boutique-inventory/boutique_inventory.rb
@@ -4,19 +4,20 @@ class BoutiqueInventory
   end
 
   def item_names
-    raise 'Implement the BoutiqueInventory#item_names method'
+    @items.map { |item| item[:name] }.sort
   end
 
   def cheap
-    raise 'Implement the BoutiqueInventory#cheap method'
+    @items.select { |item| item[:price] < 30}
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select { |item| item[:quantity_by_size].size == 0 }
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    named_item = @items.select { |item| item[:name] == name}
+    named_item.map {|item| item[:quantity_by_size]}
   end
 
   def total_stock
